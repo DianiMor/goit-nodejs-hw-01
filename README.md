ced49c337f8e99682da773b88fa5719ae0531faa
DianiMor <diana.3mol@gmail.com>
2024-05-11 01:22:38 -0500

    com1

---
+++ README.md
@@ -0,0 +1,4 @@
+https://ibb.co/GWKdp2J
+https://ibb.co/gjLZ7zq
+https://ibb.co/vsQCgBG
+https://ibb.co/HKVrRXr
---
+++ contacts.js
@@ -0,0 +1,62 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.resolve("db/contacts.json");
+
+async function listContacts() {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+    return contact || null;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: Math.random().toString(36).substring(2, 15),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
